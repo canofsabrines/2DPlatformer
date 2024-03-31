d2f2f7720f5be91cf7083cdece1a115ecb64e7d8
canofsabrines <100914279+canofsabrines@users.noreply.github.com>
2024-03-30 23:33:28 -0400

    removed first build

--- Assets/Scripts/GameEvent.cs
+++
@@ -1,9 +0,0 @@
-public static class GameEvent
-{
-    public const string COIN_OBTAINED = "COIN_OBTAINED";
-    public const string TEN_COINS = "TEN_COINS";
-    public const string GAME_WIN = "GAME_WIN";
-    public const string PLAYER_DEATH = "PLAYER_DEATH";
-    public const string BOSS_SPAWN = "BOSS_SPAWN";
-    public const string ENEMY_HIT = "ENEMY_HIT";
-}
